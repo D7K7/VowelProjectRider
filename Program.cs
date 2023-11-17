d31b1750a03423eeb99fcfc6db1bdffe01ec57d5
D7K7 <dakhjohnson@proton.me>
2023-11-17 10:44:07 -0600

    Added Project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int count = 0;
+string vowels = "aeiouAEIOU";
+string str = Console.ReadLine();
+
+foreach (char c in str)
+{
+    if (vowels.Contains(c))
+    {
+        count++;
+    }
+}
+
+return count;
